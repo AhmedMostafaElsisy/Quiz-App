d1e06d9c3360abe315df4b1a431e53e3d63d2e7c
Ahmed Mostafa <33898506+ahmedmostafa914@users.noreply.github.com>
2021-07-11 15:51:18 +0200

    add project files

---
+++ app/src/main/java/com/example/quizzapp/QuizListViewModel.java
@@ -0,0 +1,32 @@
+package com.example.quizzapp;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+public class QuizListViewModel extends ViewModel implements com.example.quizzapp.FirebaseRepository.OnFirestoreTaskComplete {
+
+    private MutableLiveData<List<QuizListModel>> quizListModelData = new MutableLiveData<>();
+
+    public LiveData<List<QuizListModel>> getQuizListModelData() {
+        return quizListModelData;
+    }
+
+    private com.example.quizzapp.FirebaseRepository firebaseRepository = new  com.example.quizzapp.FirebaseRepository(this);
+
+    public QuizListViewModel() {
+        firebaseRepository.getQuizData();
+    }
+
+    @Override
+    public void quizListDataAdded(List<QuizListModel> quizListModelsList) {
+        quizListModelData.setValue(quizListModelsList);
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+}
